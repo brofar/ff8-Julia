d443534a82bdd9c29f40abab16578499132fc434
Behfar Lotfizadeh <behfar@otafest.com>
2021-03-23 00:15:07 -0600

    Fix some async issues.

--- Form1.cs
+++ Form1.cs
@@ -18,8 +18,8 @@ namespace ff8_Julia
         static IntPtr GameBaseAddress;
         static int carawayMapId = 761;
         static int carawayMoment = 351;
+        static string carawayCode;
         static Timer t;
-        static formMain window;
 
         static Dictionary<string, int> CodeAddress = new Dictionary<string, int>
         {
@@ -76,95 +76,111 @@ namespace ff8_Julia
         }
 
 
-        private async void BeginCodeProcessing()
+        private void BeginCodeProcessing()
         {
-            // Kill the timer if it exists already
-            if (t != null)
-            {
-                t.Dispose();
-            }
-
             // Detect the game
+            SetCodeText("0");
             SetStatusText("Waiting for game to start.");
-            await Task.Run(GetGame);
-
-            // Update status
-            SetStatusText(GameVersion + " - Waiting for Caraway's Mansion scene in game.");
-
-            // Begin sniffing every 500ms
-            t = new Timer(SearchForCode, null, 0, 500);
+            DetectGame();
         }
 
-        private void GetGame()
-        {
-            bool gameFound = false;
-            while (gameFound == false)
-            {
-                gameFound = DetectGame();
-            }
-        }
 
-        private void SearchForCode(Object o)
+
+        private bool CodeCriteria()
         {
-            while (!String.IsNullOrEmpty(GameVersion))
+            bool goMode = false;
+
+            do
             {
                 // Story Moment
                 var momentAddress = MomentAddress[GameVersion];
                 var gameMoment = ReadMemoryAddress(momentAddress, 2);
 
-                bool rightMoment = (gameMoment == carawayMoment);
+                // Map ID
+                var mapIdAddress = MapIdAddress[GameVersion];
+                var mapId = ReadMemoryAddress(mapIdAddress, 2);
 
-                if (rightMoment)
-                {
-                    // Map ID
-                    var mapIdAddress = MapIdAddress[GameVersion];
-                    var mapId = ReadMemoryAddress(mapIdAddress, 2);
+                bool rightMap = (mapId == carawayMapId);
+                bool rightMoment = (gameMoment >= carawayMoment);
 
-                    bool rightMap = (mapId == carawayMapId);
+                goMode = (rightMap && rightMoment);
+            } while (goMode == false);
 
-                    if (rightMap && rightMoment)
-                    {
-                        var codeAddress = CodeAddress[GameVersion];
-                        var code = ReadMemoryAddress(codeAddress, 1);
-
-                        SetCodeText(code.ToString().PadLeft(3, '0'));
-                        SetStatusText("Mansion Code found.");
-                    }
-                    else
-                    {
-                        SetCodeText("0");
-                        SetStatusText(GameVersion + ": Waiting for mansion scene.");
-                    }
-                }
-            }
+            return goMode;
         }
-
-        private bool DetectGame()
+        private async void DetectGame()
         {
             // Find the FF8 process.
             Console.WriteLine("Checking for game...");
-            List<Process> processes = Process.GetProcesses()
+            Process ff8Game = await Task.Run(FindGame);
+            Console.WriteLine("Game found.");
+
+            // Get the language from the process name (i.e. remove "FF8_" from the name)
+            GameVersion = ff8Game.ProcessName.Substring(4);
+            GameBaseAddress = ff8Game.MainModule.BaseAddress;
+
+            // Add event handler for exited process
+            ff8Game.EnableRaisingEvents = true;
+            ff8Game.Exited += new EventHandler(myprc_Exited);
+
+            FF8Process = ff8Game;
+
+            // Update status
+            SetStatusText(GameVersion + " - Waiting for Caraway's Mansion scene in game.");
+
+            bool codeCriteria = await Task.Run(CodeCriteria);
+
+
+            t = new Timer(SearchForCode, null, 0, 500);
+            //GetCode();
+
+        }
+
+        private void SearchForCode(object state)
+        {
+            // Story Moment
+            var momentAddress = MomentAddress[GameVersion];
+            var gameMoment = ReadMemoryAddress(momentAddress, 2);
+
+            // Map ID
+            var mapIdAddress = MapIdAddress[GameVersion];
+            var mapId = ReadMemoryAddress(mapIdAddress, 2);
+
+            bool rightMap = (mapId == carawayMapId);
+            bool rightMoment = (gameMoment >= carawayMoment);
+
+            if(rightMap && rightMoment)
+            {
+                GetCode();
+            }
+        }
+
+        private void GetCode()
+        {
+            var codeAddress = CodeAddress[GameVersion];
+            var code = ReadMemoryAddress(codeAddress, 1);
+            var codeString = code.ToString().PadLeft(3, '0');
+
+            if(carawayCode != codeString)
+            {
+                carawayCode = codeString;
+                SetCodeText(codeString);
+                SetStatusText("Mansion Code found.");
+            }
+        }
+
+        private Process FindGame()
+        {
+            List<Process> processes = new List<Process>();
+            do
+            {
+                processes = Process.GetProcesses()
                 .Where(x => (x.ProcessName.StartsWith("FF8_", StringComparison.OrdinalIgnoreCase))
                             && !(x.ProcessName.Equals("FF8_Launcher", StringComparison.OrdinalIgnoreCase)))
                 .ToList();
+            } while (processes.Count == 0);
 
-            if (processes.Count > 0)
-            {
-                Process ff8Game = processes[0];
-                Console.WriteLine("Game found.");
-
-                // Get the language from the process name (i.e. remove "FF8_" from the name)
-                GameVersion = ff8Game.ProcessName.Substring(4);
-                GameBaseAddress = ff8Game.MainModule.BaseAddress;
-
-                // Add event handler for exited process
-                ff8Game.EnableRaisingEvents = true;
-                ff8Game.Exited += new EventHandler(myprc_Exited);
-
-                FF8Process = ff8Game;
-                return true;
-            }
-            return false;
+            return processes[0];
         }
 
         private int ReadMemoryAddress(int offset, uint bytelength)
@@ -214,6 +230,10 @@ namespace ff8_Julia
 
         private void myprc_Exited(object sender, EventArgs e)
         {
+            // Kill the timer if it exists already
+            if (t != null)
+                t.Dispose();
+
             BeginCodeProcessing();
         }
 
